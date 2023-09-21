f047bfd7642c52f1f593b1e21431ac83da01be20
Casey Vu <143982295+calih3atwave@users.noreply.github.com>
2023-09-20 20:50:48 -0700

    Created function to remove elements from an array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function (inputArray, ...args) {
+  /**
+   * Returns copy of inputArray without the target elements.
+   */
+  // Create a new empty array
+  let outputArray = [];
 
+  // use for Each to go through the array
+  inputArray.forEach((currentElement) => {
+    // push every element into the new array
+    // UNLESS it is included in the function arguments
+    // so we create a new array with every item, except those that
+    // should be removed
+    if (!args.includes(currentElement)) {
+      outputArray.push(currentElement);
+    }
+  });
+  return outputArray;
 };
 
 // Do not edit below this line
