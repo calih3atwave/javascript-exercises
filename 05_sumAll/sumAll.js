cb87ddadcf8fbdf3042f31ff43e2d481fd4332e6
Casey Vu <143982295+calih3atwave@users.noreply.github.com>
2023-09-20 21:18:18 -0700

    Created a function that takes 2 integers and returns the sum  of  every number between and including them.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  // Return ERROR if either value is not a number
+  if (!Number.isInteger(start) || !Number.isInteger(end)) {
+    return "ERROR";
+  }
 
+  // Return ERROR if either value is negative
+  if (start < 0 || end < 0) {
+    return "ERROR";
+  }
+
+  // Swap end with start if end is larger than start
+  if (start > end) {
+    let temp = start;
+    start = end;
+    end = temp;
+  }
+  let outputSum = 0;
+  for (let i = start; i <= end; i++) {
+    outputSum += i;
+  }
+  return outputSum;
 };
 
 // Do not edit below this line
