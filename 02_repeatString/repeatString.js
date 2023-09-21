91e4a62e2da9a3123195181298fd0720e3a7f597
Casey Vu <143982295+calih3atwave@users.noreply.github.com>
2023-09-20 20:49:29 -0700

    Created function to repeat a string.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (inputString, count) {
+  // Return ERROR if the count is negative
+  if (count < 0) {
+    return "ERROR";
+  }
+  // Build the string
+  let outputString = "";
+  // Concatenate the strings count number of times
+  for (let i = 0; i < count; i++) {
+    outputString += inputString;
+  }
+  return outputString;
 };
 
 // Do not edit below this line
